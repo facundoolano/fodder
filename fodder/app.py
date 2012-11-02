1a4b19c1e0e4f5621265ab9c400c8969b600b771
Facundo Olano <facundo.olano@gmail.com>
2012-11-02 16:08:58 -0300

    folder reorganization for flask

--- fodder/app.py
+++ fodder/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask.templating import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
